943ea6ab181f836a4748276b1cc402497a8a0f4f
Matheus Vinicius Passos de Santana <83960684+matheusvps@users.noreply.github.com>
2023-03-13 08:16:06 -0300

    Add files via upload
    Desafio 2 do Curso 
    Foi criado um "ToDo". Um bloco de tarefas pendentes/concluídas com barra de carregamento e comportamento dinâmico.
    Existe a possibilidade de excluir tarefas apertando o botão X e alterar o estado dela (feito/não feito) clicando na tarefa.
    Utilizou-se Local Storage para que as informações do usuário não fossem perdidas após reinicializar o browser .

---
+++ projeto2-tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <Task v-for="(task, i) in tasks" :key="task.name"
+                @taskDeleted="$emit('taskDeleted', i)"
+                @taskStateChanged="$emit('taskStateChanged', i)"
+                :task="task"></Task>
+        </template>
+        <p v-else class="no-task"> Sem tarefas por fazer </p>
+    </div>
+</template>
+
+<script>
+import Task from './TaskArch.vue'
+
+export default {
+    components: { Task },
+    props: {
+        tasks: { type: Array, required: true }
+    }
+}
+</script>
+
+<style>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .task-grid .task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #AAA;
+        font-size: 1.7rem;
+    }
+</style>
